4bbba1341e6081bc0ac183e619f9ccff19792fe0
KandeSoto <kandelariosoto@gmail.com>
2022-10-19 15:39:04 -0700

    acomodo de archivos y carpetas

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -2,26 +2,18 @@ import React from "react";
 import "./TodoItem.css";
 
 function TodoItem(props) {
-  const onComplete = () => {
-    alert("Ya completaste el todo " + props.text);
-  };
-
-  const onDelete = () => {
-    alert("Borraste el todo " + props.text);
-  };
-
   return (
     <li className="TodoItem">
       <span
         className={`Icon icon-check ${props.completed && "Icon-check--active"}`}
-        onClick={onComplete}
+        onClick={props.onComplete}
       >
         √
       </span>
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
         {props.text}
       </p>
-      <span onClick={onDelete} className="Icon Icon-delete">
+      <span onClick={props.onDelete} className="Icon Icon-delete">
         X
       </span>
     </li>
